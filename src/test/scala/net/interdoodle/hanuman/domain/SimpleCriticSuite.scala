bdff9a705e48905f867884ce17942e6ebac535e0
Mike Slinn <mslinn@mslinn.com>
2011-11-21 15:22:29 -0800

    Comparing JSON properly now

--- src/test/scala/net/interdoodle/hanuman/domain/SimpleCriticSuite.scala
+++ src/test/scala/net/interdoodle/hanuman/domain/SimpleCriticSuite.scala
@@ -3,7 +3,8 @@ package net.interdoodle.hanuman.domain
 import org.scalatest.Assertions._
 import org.scalatest.FunSuite
 import net.interdoodle.hanuman.message.TextMatch
-
+import blueeyes.json.JsonAST.JObject
+import blueeyes.json.Printer
 
 /**
  * @author Mike Slinn */
@@ -25,18 +26,15 @@ class SimpleCriticSuite extends FunSuite {
 
   test("assessText") {
     sCritic.assessText(document, null, "ab")
-    assert(sCritic.textMatch===TextMatch(null, 2, 0, 2))
-
-    // Generates: JObject(List(JField(monkeyRef,JString(Null workCellRef)), JField(length,JInt(2)), JField(startPos,JInt(0)), JField(endPos,JInt(2))))
-    // Instead I want actual JSON, something like:
-    // {"result":[{"monkeyRef":"b4a73ed1-0bd3-11e1-ab88-485b3988902c","length":4,"startPos":587,"endPos":591},{"monkeyRef":"b4a73ed4-0bd3-11e1-ab88-485b3988902c","length":5,"startPos":88053,"endPos":58} }
-    // What is the simplest way to emit the JSON so it can be compared to expected results?
-    println(sCritic.textMatch.decompose)
+    var actual = Printer.compact(Printer.render(sCritic.textMatch.decompose)).toString
+    assert(actual==="""{"monkeyRef":"Null workCellRef","length":2,"startPos":0,"endPos":2}""")
 
     sCritic.assessText(document, null, "_abcd_")
-    assert(sCritic.textMatch===TextMatch(null, 4, 1, 5))
+    actual = Printer.compact(Printer.render(sCritic.textMatch.decompose)).toString
+    assert(actual==="""{"monkeyRef":"Null workCellRef","length":4,"startPos":3,"endPos":5}""")
 
     sCritic.assessText(document, null, "ab_ab__abcd_abcdefgh")
-    assert(sCritic.textMatch===TextMatch(null, 8, 12, 20))
+    actual = Printer.compact(Printer.render(sCritic.textMatch.decompose)).toString
+    assert(actual==="""{"monkeyRef":"Null workCellRef","length":8,"startPos":12,"endPos":20}""")
   }
 }
