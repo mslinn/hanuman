cc6c9bf8ff9940183321cc9eea85384ef2028a5b
Mike Slinn <mslinn@micronauticsresearch.com>
2011-11-26 08:07:34 -0800

    Tweaking stylesheet

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,6 +1,7 @@
 $(function() {
     var simulationId;
     var debug = false;
+    var running = false;
 
     function createSimulation() {
         $.ajax("/newSimulation", {
@@ -22,8 +23,10 @@ $(function() {
     }
 
     function runSimulation() {
+        running = true;
         if (debug==true)
-            $("#debug").append("runSimulation(): About to invoke run<br/>\n");
+            $("#debug").show()
+        $("#debug").append("runSimulation(): About to invoke run<br/>\n");
         $.ajax("/run/" + simulationId, {
             contentType: "application/json",
             success: onRunSuccess,
@@ -32,8 +35,7 @@ $(function() {
     }
 
     function onError(jqXHR, textStatus, errorThrown) {
-        if (debug==true)
-            $("#debug").append("onError: " + textStatus + " "  + errorThrown +"<br/>\n")
+        $("#debug").append("onError: " + textStatus + " "  + errorThrown +"<br/>\n")
     }
 
     function onRunSuccess(data) {
@@ -47,8 +49,7 @@ $(function() {
     }
 
     function getSimulationStatus() {
-        if (debug==true)
-            $("#debug").append("<br/>\ngetSimulationStatus(): About to invoke status<br/>\n");
+        $("#debug").append("getSimulationStatus(): About to invoke status<br/>\n");
         $.ajax("/status/" + simulationId, {
             contentType: "application/json",
             success: onGetSimulationStatus,
@@ -78,7 +79,9 @@ $(function() {
     }
 
     function stopSimulation() {
+        running = false;
         $("#debug").html();
+        $("#debug").hide();
         $("#results").hide();
         $("#stopSimulationButton").hide();
         $("#newSimulationButton").show();
@@ -87,15 +90,25 @@ $(function() {
         });
     }
 
+    function toggleDebug(){
+        debug = !debug;
+        if (debug==true && running==true)
+           $("#debug").show("slow");
+        else
+           $("#debug").hide("slow");
+    }
+
     $("body")
         .append('<h1 style="font-family: arial">Hanuman <span id="version"></span></h1>\n')
         .append('<p><a href="https://github.com/mslinn/hanuman" target="src">Source on GitHub</a><br/>' +
                 '<a href="http://www.slideshare.net/mslinn/hanuman-10278606" target="vid">Presentation on SlideShare</a></p>\n')
         .append('<button id="newSimulationButton">Start a new simulation</button>\n')
-        .append('<div id="results"></div>\n')
-        .append('<div id="debug"></div>\n')
+        .append('<div id="results" class="results"></div>\n')
+        .append('<div id="debug" class="debug"></div>\n')
         .append('<a href="http://micronauticsresearch.com" target="mr"><img src="http://micronauticsresearch.com/images/logo-300x91.png" style="position: absolute; bottom: 5; left: 5" /></a>\n')
+        .append('<div class="debugCheckbox">Debug <input type="checkbox" id="debugCheckbox" />\n</div>\n')
         .append('<a href="http://heroku.com" target="h"><img src="https://nav.heroku.com/images/logos/logo.png" style="position: absolute; bottom: 5; right: 5" /></a>\n');
+    $("#debug").hide()
     $("#results").append('<span class=label>Simulation ID</span> <tt>' + simulationId + '</tt> &nbsp;&nbsp;')
                  .append('<button id="stopSimulationButton">Stop simulation</button>\n')
                  .append('<br/><br/>\n')
@@ -104,4 +117,5 @@ $(function() {
                  .append('<span id="match">0</span> characters matched so far:<br/>\n')
                  .append('<div id="matchedPortion" class="matchedPortion"></div>\n').hide();
     $("#newSimulationButton").click(createSimulation);
+    $("#debugCheckbox").click(toggleDebug);
 })
